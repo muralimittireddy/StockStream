339ff56258d6504bdd8efbe92f35210dad9fdf12
murali <muralimittireddy1234@gmail.com>
2025-04-28 23:58:11 +0530

    connected Spark Structured Streaming consumer to ClickHouse database successfully

---
+++ consumer/clickhouse_client.py
@@ -0,0 +1,59 @@
+# clickhouse_client.py
+
+from clickhouse_connect import get_client
+
+# Connection setup
+client = get_client(
+    host='clickhouse',
+    port=8123,  # HTTP port for ClickHouse
+    username='user',
+    password='user_password'
+)
+
+# Create table if not exists
+def create_ohlcv_table():
+    # Step 1: Create database if it doesn't exist
+    client.command('CREATE DATABASE IF NOT EXISTS stock')
+    print("database created")
+
+    # Step 2: Switch to stock database
+    client.command('USE stock')
+    print("its in use")
+
+     # Step 3: Create table if it doesn't exist
+    client.command('''
+    CREATE TABLE IF NOT EXISTS ohlcv_data (
+        timestamp DateTime,
+        symbol String,
+        open Float64,
+        high Float64,
+        low Float64,
+        close Float64,
+        volume Float64,
+        category String
+    ) ENGINE = MergeTree()
+    ORDER BY (symbol, timestamp)
+    ''')
+    print("ClickHouse table is ready.")
+
+# Insert batch into ClickHouse
+def insert_ohlcv_batch(batch_data):
+    rows = []
+    for record in batch_data:
+        rows.append((
+            record['timestamp'],
+            record['symbol'],
+            record['open'],
+            record['high'],
+            record['low'],
+            record['close'],
+            record['volume'],
+            record.get('category', '')
+        ))
+    
+    client.insert(
+        table='stock.ohlcv_data',
+        data=rows,
+        column_names=['timestamp', 'symbol', 'open', 'high', 'low', 'close', 'volume','category']
+    )
+    print(f"Inserted {len(rows)} records into ClickHouse.")
--- consumer/consumer.py
+++ consumer/consumer.py
@@ -3,9 +3,17 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, from_json
 from pyspark.sql.types import StructType, StructField, StringType, FloatType, TimestampType
 
+from clickhouse_client import create_ohlcv_table, insert_ohlcv_batch
+
+packages = [
+    "com.clickhouse.spark:clickhouse-spark-runtime-3.3_2.12:0.8.0",
+    "org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.3",
+    "com.clickhouse:clickhouse-jdbc:0.6.3"]
+
 # Initialize Spark Session
 spark = SparkSession.builder \
     .appName("Kafka_Spark_Consumer") \
+    .config("spark.jars.packages", ",".join(packages)) \
     .getOrCreate()
 
 # Set log level to WARN to reduce unnecessary logs
@@ -14,6 +22,9 @@ spark.sparkContext.setLogLevel("WARN")
 # Define Kafka topics and Kafka broker
 kafka_broker = "broker:29092"
 kafka_topics = ["stocks", "crypto_currency", "indices", "etfs", "currencies"]
+
+# Create table before starting
+create_ohlcv_table()
 
 # Define schema for the incoming data
 schema = StructType([
@@ -32,23 +43,53 @@ kafka_stream_df = spark.readStream \
     .option("subscribe", ",".join(kafka_topics)) \
     .load()
 
-# The Kafka message is in the 'value' field, which is in binary format.
+# Kafka message is in the 'value' field, which is in binary format.
 # Deserialize the Kafka message from binary to string and apply the schema
-# Extract the 'value' field and cast it to string
-df = kafka_stream_df.selectExpr("CAST(value AS STRING) as json_value")
+df = kafka_stream_df.selectExpr("CAST(value AS STRING) as json_value","topic")
 
 # Parse the JSON string using from_json
-df_json = df.select(from_json(col("json_value"), schema).alias("data")).select("data.*")
+df_json = df.select(from_json(col("json_value"), schema).alias("data"),
+    col("topic"))
+
+# Flatten data
+df_json_flattened = df_json.select(
+    "data.*",
+    "topic"
+)
+
+# Add 'category' column, which is derived from the Kafka topic
+df_json_with_category = df_json.withColumn("category", col("topic"))
+
+# Now select the necessary columns, including the new 'category' field
+processed_df = df_json_with_category.select(
+    "data.timestamp",
+    "data.symbol",
+    "data.open",
+    "data.high",
+    "data.low",
+    "data.close",
+    "data.volume",
+    "category"
+)
+
+def write_to_clickhouse(batch_df, batch_id):
+    batch_df.write \
+        .format("jdbc") \
+        .option("url", "jdbc:clickhouse://clickhouse:8123/stock") \
+        .option("driver", "com.clickhouse.jdbc.ClickHouseDriver") \
+        .option("dbtable", "ohlcv_data") \
+        .option("user", "user") \
+        .option("password", "user_password") \
+        .mode("append") \
+        .save()
 
 # For demo purposes, let's display the data
 # You can apply transformations, filtering, and processing as required
-processed_df = df_json.select("symbol", "timestamp", "open", "high", "low", "close", "volume")
-
-# Write the processed data to the console (you can also write it to other sinks like files, databases, etc.)
 query = processed_df.writeStream \
-    .outputMode("append") \
-    .format("console") \
-    .start()
+            .foreachBatch(write_to_clickhouse) \
+            .outputMode("append") \
+            .start()
+
 
 # Await termination of the query
 query.awaitTermination()
